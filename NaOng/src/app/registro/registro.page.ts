dfcf0b1e7e2060a22c13ff2d28634246c3f6cd26
Joao <joaov.cercena@gmail.com>
2023-11-27 23:47:04 -0300

    crud registro

--- NaOng/src/app/registro/registro.page.ts
+++ NaOng/src/app/registro/registro.page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-registro',
@@ -9,53 +11,141 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage {
-  ong: number = 0;
-  id_doador: number = 0;
+  id_ong: number = 0;
   descricao: string = '';
   setor: string = '';
   data: string = '';
   listOng: any = [];
   idRecebido: any;
   valor: number = 0.0;
+  listRegistro: any = [];
+  id_registro: number = 0;
 
-  constructor(private http: HttpClient, private navCtrl: NavController, private activatedRoute: ActivatedRoute, private router: Router) {
+  constructor(private http: HttpClient, public alertController: AlertController, private activatedRoute: ActivatedRoute, private router: Router) {
     this.activatedRoute.queryParams.subscribe(params => {
       this.idRecebido = params['id'];
-      console.log(this.idRecebido);
+
+      if (params['id_registro']) {
+        this.id_registro = params['id_registro'];
+
+        this.http.get<any>(`http://localhost:3000/registro?id=${this.id_registro}`).subscribe(
+          (dados) => {
+            this.data = dados[0].data;
+            this.id_ong = dados[0].id_ong;
+            this.descricao = dados[0].descricao;
+            this.valor = dados[0].valor;
+            this.setor = dados[0].setor;
+
+          },
+          (error) => {
+            console.error('Erro ao buscar registro:', error);
+          }
+        );
+      }
     });
   }
 
   ionViewWillEnter() {
-    this.listar();
+    this.listarOng();
+    this.listarRegistro();
   }
 
   showHome() {
-    this.navCtrl.navigateBack('home')
+    this.router.navigate(['home'], {
+      queryParams: { id: this.idRecebido },
+      relativeTo: this.activatedRoute.parent
+    });
+  }
+
+  editaRegistro(id_registro: any) {
+
+    this.router.navigate(['registro'], {
+      queryParams: { id: this.idRecebido, id_registro: id_registro },
+      relativeTo: this.activatedRoute.parent
+    });
   }
 
   cadastrar() {
     const novoRegistro = {
-      ong: this.ong,
+      id_ong: this.id_ong,
       id_doador: this.idRecebido,
       descricao: this.descricao,
       setor: this.setor,
       data: this.data,
       valor: this.valor,
     };
-    console.log(this.idRecebido)
 
-    // Envia os dados para o servidor JSON
-    this.http.post('http://localhost:3000/registro', novoRegistro).subscribe(
-      (data) => {
-        console.log('Registro cadastrado com sucesso:', data);
-        this.navCtrl.navigateBack('home'); // Redireciona para a página 'home' após o cadastro
-      },
-      (error) => {
-        console.error('Erro ao cadastrar registro:', error);
+    if (!this.id_ong || !this.idRecebido || !this.descricao || !this.setor || !this.data) {
+      this.alertController.create({
+        header: 'AVISO!',
+        message: 'Digite todos os campos!',
+        buttons: ['OK']
+      }).then(alert => {
+        alert.present();
+      });
+    } else {
+
+      // Envia os dados para o servidor JSON
+      if (!this.id_registro) {
+        this.http.post('http://localhost:3000/registro', novoRegistro).subscribe(
+          (data) => {
+            console.log('Registro cadastrado com sucesso:', data);
+            this.showHome();
+          },
+          (error) => {
+            console.error('Erro ao cadastrar registro:', error);
+          }
+        );
+      } else {
+        this.http.put(`http://localhost:3000/registro/${this.id_registro}`, novoRegistro).subscribe(
+          (data) => {
+            this.alertController.create({
+              header: 'CADASTRADO!',
+              message: 'Cadastrado com sucesso!',
+              buttons: ['OK']
+            }).then(alert => {
+              alert.present();
+            });
+            this.showHome();
+          },
+          (error) => {
+            console.error('Erro ao cadastrar registro:', error);
+          }
+        );
       }
-    );
+    }
   }
-  listar() {
+
+  deletar() {
+    if (this.id_registro) {
+      this.http.delete(`http://localhost:3000/registro/${this.id_registro}`).subscribe(
+        (data) => {
+          this.alertController.create({
+            header: 'EXCLUIDO!',
+            message: 'Registro excluído',
+            buttons: ['OK']
+          }).then(alert => {
+            alert.present();
+          });
+          this.showHome();
+        },
+        (error) => {
+          console.error('Erro ao deletar registro:', error);
+        }
+      );
+    } else {
+      this.alertController.create({
+        header: 'AVISO!',
+        message: 'Selecio um registro',
+        buttons: ['OK']
+      }).then(alert => {
+        alert.present();
+      });
+
+    }
+  }
+
+  listarOng() {
     // Envia os dados para o servidor JSON
     this.http.get('http://localhost:3000/ong').subscribe(
       (data) => {
@@ -63,6 +153,17 @@ export class RegistroPage {
       },
       (error) => {
         console.error('Erro ao buscar ong:', error);
+      }
+    );
+  }
+  listarRegistro() {
+    // Envia os dados para o servidor JSON
+    this.http.get<any>(`http://localhost:3000/registro?id_doador=${this.idRecebido}`).subscribe(
+      (data) => {
+        this.listRegistro = data;
+      },
+      (error) => {
+        console.error('Erro ao buscar registro:', error);
       }
     );
   }
